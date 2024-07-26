b345d2ee015cffbf42ebe8413ec08c46b423c90d
Skana <87369910+SkanaIS@users.noreply.github.com>
2024-07-26 14:04:05 +0800

    V1

---
+++ api.js
@@ -0,0 +1,62 @@
+const rp = require('request-promise');
+const post_url = 'http://mc.skana.fun:5700'
+function root_func(path,body){
+    const options = {method: 'POST', url: post_url + path,body: body,json: true};
+    return (rp(options))
+}
+/**获取群信息
+ * 终结点：/get_group_info
+ * 提示: 如果机器人尚未加入群, group_create_time, group_level, max_member_count 和 member_count 将会为0
+ * @param {number} group_id 群号
+ * @param {boolean} no_cache 是否不使用缓存（使用缓存可能更新不及时, 但响应更快）
+ */
+function API_get_group_info(group_id, no_cache = false) {
+    let body= {'group_id': group_id,'no_cache': no_cache}
+    return root_func('/get_group_info',body)
+}
+/**发送消息
+ * 终结点：/send_msg
+ * @param {string} message_type 消息类型, 支持 private、group , 分别对应私聊、群组, 如不传入, 则根据传入的 *_id 参数判断 
+ * @param {number} user_id 对方 QQ 号 ( 消息类型为 private 时需要 )
+ * @param {number} group_id 群号 ( 消息类型为 group 时需要 )
+ * @param {string} message 要发送的内容
+ * @param {boolean} auto_escape 消息内容是否作为纯文本发送 ( 即不解析 CQ 码 ) , 只在 message 字段是字符串时有效
+ * @returns {object}
+ */
+function API_send_msg(message_type, user_id, group_id, message, auto_escape = false) {
+    let body ={'message_type': message_type,'message': message,'auto_escape': auto_escape}
+    message_type = 'private' ? body['user_id'] = user_id : body['group_id'] = group_id;
+    return root_func('/send_msg',body)
+}
+/**发送私聊消息
+ * 终结点：/send_private_msg
+ * @param {number} user_id 对方 QQ 号
+ * @param {number} group_id 主动发起临时会话时的来源群号(可选, 机器人本身必须是管理员/群主)
+ * @param {string} message 要发送的内容
+ * @param {boolean} auto_escape 消息内容是否作为纯文本发送 ( 即不解析 CQ 码 ) , 只在 message 字段是字符串时有效
+ */
+function API_send_private_msg(user_id, group_id, message, auto_escape) {
+    let body ={'user_id': user_id,'group_id': group_id,'message': message,'auto_escape':auto_escape}
+    return root_func('/send_private_msg',body)
+}
+/**发送群聊消息
+ * /send_group_msg
+ * @param {number} group_id 
+ * @param {string} message 
+ * @param {boolean} auto_escape 
+ * @returns 
+ */
+function API_send_group_msg(group_id, message, auto_escape) {
+    let body ={'group_id': group_id,'message': message,'auto_escape':auto_escape}
+    return root_func('/send_group_msg',body)
+}
+
+function API_set_group_portrait(){
+    
+}
+module.exports = {
+    API_get_group_info,
+    API_send_msg,
+    API_send_private_msg,
+    API_send_group_msg
+}
---
+++ index.js
@@ -0,0 +1,75 @@
+const WebSocket = require('ws')
+const api = require('./api.js')
+const ws = new WebSocket(process.env.CQServer)
+
+function QQMessagePost(dataJson){
+    this.time=dataJson['time']
+    this.self_id=dataJson['self_id']
+    this.message_type=dataJson['message_type'];//消息类型(string):private,group
+    this.sub_type=dataJson['sub_type'];//表示消息的子类型(string):group,public
+    this.message_id=dataJson['message_id'];//消息ID(int32)
+    this.user_id=dataJson['user_id'];//发送者QQ号(int64)
+    this.message=dataJson['message'];
+    this.raw_message=dataJson['raw_message'];
+    this.sender=new QQSender(dataJson['sender'])
+    //私聊
+    if(this.message_type=='private'){this.target_id=dataJson['target_id'];this.temp_source=dataJson['temp_source']}
+    //群聊
+    if(this.message_type=='group'){this.group_id=dataJson['group_id']}
+}
+function QQSender(sender){
+    this.user_id=sender['user_id'];
+    this.nickname=sender['nickname'];
+    this.sex=sender['sex'];
+    this.card=sender['card'];
+    this.area=sender['area'];
+    this.level=sender['level'];
+    this.role=sender['role'];
+    this.title=sender['title'];
+}
+function QQGroupInfo(data){
+    this.group_id=data['group_id'];
+    this.group_name=data['group_name'];
+    this.group_memo=data['group_memo'];
+    this.group_create_time=data['group_create_time'];
+    this.group_level=data['group_level'];
+    this.group_count=data['group_count'];
+    this.group_member_count=data['group_member_count'];
+}
+ws.on('open', function open()
+{
+    console.log('Connect');
+});
+
+ws.on('close', function close()
+{
+  console.log('disconnected');
+});
+
+ws.on('message', function message(data) 
+{
+    const DataJson = JSON.parse(data.toString());
+    const DataPostType=DataJson['post_type'];
+
+    if(DataPostType=='meta_event'){return}//心跳
+    switch(DataPostType){//判断消息类型
+        case 'message':
+        case 'message_sent':
+            const Message=new QQMessagePost(DataJson);
+            switch(Message.message_type){
+                case 'group':
+                    api.API_get_group_info(Message.group_id).then(result=>{
+                        const Groupinfo = new QQGroupInfo(result['data'])
+                        console.info(`收到消息:来自群${Groupinfo.group_name}(${Groupinfo.group_id})的${Message.sender.nickname}(${Message.sender.user_id}):${Message.raw_message}`);
+                        console.debug(Message.message)
+                    })
+                    break;
+                case 'private':
+                    console.info(`收到消息:来自${Message.sender.nickname}(${Message.sender.user_id}):${Message.raw_message}`);
+                    break;
+            }
+            break;
+        case 'request':
+            console.log(dataJson['request_type'])
+    }
+});
